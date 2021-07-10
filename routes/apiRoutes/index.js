08211d2e91a1ad0bc9bb830d33def8f91c497949
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-10 15:17:17 -0500

    finalize pseudo code for file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,4 +2,5 @@
 // import data from /db/db
 // import express.Router
 // create get and post route for /notes
+// --- in the post route we'll need to find a way to create a unique id for the note
 // export router
