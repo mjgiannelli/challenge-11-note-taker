b532fc139b2688926ab2fa05ae6dcf5093826bf1
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-10 14:26:51 -0500

    create pseudo code for server

---
+++ server.js
@@ -0,0 +1,8 @@
+// pull in express package
+// create a variable for PORT
+// declare app and set to equal express method
+// parse income string or array data
+// parse incoming JSON
+// use express static on public folder
+// use the routes
+// create app.listen to variable PORT
