467cd9a0db0944ffae31642bd77fb5e49e65d741
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-10 15:39:57 -0500

    complete server.js file

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 // pull in express package
+const express = require('express')
 // create a variable for PORT
+const PORT = process.env.PORT || 3001;
 // declare app and set to equal express method
+const app = express();
+// import the routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 // parse income string or array data
+app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON
+app.use(express.json());
 // use express static on public folder
+app.use(express.static('public'));
 // use the routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 // create app.listen to variable PORT
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
