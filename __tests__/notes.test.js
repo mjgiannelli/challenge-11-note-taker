4584693bafb43ae60d72495fee4d8cccfc39dd9e
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-11 16:31:27 -0500

    test and create function that deletes a note by ID #

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 // import functions from /lib/notes
 const {
     createNewNote,
-    validateNote
+    validateNote,
+    deleteNote
 } = require('../lib/notes.js')
 // import data from /db/db
 const { db } = require("../db/db");
@@ -37,4 +38,36 @@ test('validates note text', () => {
 
     expect(result).toBe(true);
     expect(result2).toBe(false);
+})
+
+test('delete note object using id', () => {
+    let notesArray = [
+        {
+            title: "TODO: Delete",
+            text: "Need to figure out how to delete notes",
+            id: "cl68K_hWQu_HGjEbnYVz1"
+        },
+        {
+            title: "TODO: Show Notes",
+            text: "Get notes to show up on side",
+            id: "7VmsEzTbk6OEh1mU5LzFS"
+        },
+        {
+            title: "test note",
+            text: "test text",
+            id: "2e5C2xgvMY_iQZi3rZApT"
+        }
+    ];
+
+    let deletedNoteId = '7VmsEzTbk6OEh1mU5LzFS';
+
+    deleteNote(deletedNoteId, notesArray)
+
+    expect(notesArray).not.toEqual(expect.arrayContaining([
+        expect.objectContaining({
+            id: deletedNoteId
+        })
+    ])
+    );
+
 })
