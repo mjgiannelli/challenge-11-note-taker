9143aa031665a109b83832ea9d868a67533b35ab
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-07-10 22:50:23 -0500

    add api-routes, install nanoid to generate unique id for new notes posted

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,25 @@
 // import functions from /lib/notes
+const { createNewNote, validateNote } = require('../../lib/notes');
+//import nano ID
+import { nanoid } from 'nanoid';
 // import data from /db/db
+const { notes } = require('../../db/notes');
 // import express.Router
+const router = require('express').Router();
 // create get and post route for /notes
-// --- in the post route we'll need to find a way to create a unique id for the note
+
+router.post('/notes', (req, res) => {
+    //code to create unique Id for each note
+    req.body.id = nanoid();
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted or missing information.');
+    } else {
+        // add note to json file and notes array in this function
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+})
+
 // export router
+module.exports = router;
